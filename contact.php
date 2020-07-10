010f97df75ad40da3cbbd62c6f23324bb51a8204
abhibhaw <asthana3110@gmail.com>
2020-07-10 18:12:58 +0530

    Contact.php Post moddified
    Trial-1:

--- contact.php
+++ contact.php
@@ -1,17 +1,18 @@
 <?php
 //Variable Setting
 
-$name = $_REQUEST['firstname'];
-$message = $__REQUEST['message'];
+    $name = $_POST['firstname'];
+    $email = $_POST['email']
+    $message = $__POST['message'];
 
-if (empty($name) || empty($message))
-{
-    echo "Please fill again.";
-}
-else
-{
-    mail("abhibhaw3110@gmail.com", "New Message", $message);
-    echo "(<script type = 'text/javascript'>alert('Your message sent')</script>";
-}
+    if (empty($name) || empty($message) || empty($email)
+    {
+        echo "Please fill again.";
+    }
+    else
+    {
+        mail("abhibhaw3110@gmail.com","New Message",$message);
+        echo "(<script type = 'text/javascript'>alert('Your message sent')</script>";
+    }
 
 ?>
