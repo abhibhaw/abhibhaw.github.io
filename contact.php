b0fa20a4b4b5e4e1d4eee9b719f0691044d9f3f8
abhibhaw <asthana3110@gmail.com>
2020-11-09 14:00:48 +0530

    Wrapping UP

--- contact.php
+++ contact.php
@@ -5,14 +5,5 @@
     $email = $_POST['email']
     $message = $__POST['message'];
 
-    if (empty($name) || empty($message) || empty($email)
-    {
-        echo "Please fill again.";
-    }
-    else
-    {
-        mail("abhibhaw3110@gmail.com","New Message",$message);
-        echo "(<script type = 'text/javascript'>alert('Your message sent')</script>";
-    }
-
+    mail("abhibhaw3110@gmail.com","Msg: $email",$message);
 ?>
